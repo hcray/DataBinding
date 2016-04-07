ec6333834b29d7f6d7932fae440de82e0d4e1608
unknown <21829@dahuatech.com>
2016-04-07 11:35:15 +0800

    Initial commit

---
+++ app/src/main/java/com/daoliuhe/demo/databinding/bean/User.java
@@ -0,0 +1,12 @@
+package com.daoliuhe.demo.databinding.bean;
+
+/**
+ * Created by 21829 on 2016/4/7.
+ */
+public class User {
+    private String name;
+
+    private String age;
+
+    private String address;
+}
