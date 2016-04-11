2ef05a8ea606e59a9168314b235c06633f73e9be
caiyingying <caiyingying234@gmail.com>
2016-04-11 20:01:36 +0800

    init....

--- app/src/main/java/com/daoliuhe/demo/databinding/bean/User.java
+++ app/src/main/java/com/daoliuhe/demo/databinding/bean/User.java
@@ -4,9 +4,28 @@ package com.daoliuhe.demo.databinding.bean;
  * Created by 21829 on 2016/4/7.
  */
 public class User {
-    private String name;
+    private String firstName;
 
-    private String age;
+    private String lastName;
 
-    private String address;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
